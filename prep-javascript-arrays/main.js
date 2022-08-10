000253de895c97b3c7e6953134e23c44ffc826da
Novi Le <novimacle@gmail.com>
2022-08-10 01:53:39 +0000

    Completed prep-javascript-arrays exercise

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,19 @@
+const languages = ['html',
+  'css',
+  'javascript',
+  'react',
+  'node',
+  'postgres'
+];
+console.log('Languages', languages);
+
+var firstElement = languages[0];
+console.log('the first element of the array is', firstElement);
+
+var thirdElement = languages[2];
+console.log('the third element of the array is', thirdElement);
+
+console.log('the length of the array is', languages.length);
+
+const lastElement = languages[languages.length - 1];
+console.log('the last element of the array is', lastElement);
