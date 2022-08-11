b71ef0d32a92a6360f061c7ccde365fcd66e3d51
Novi Le <novimacle@gmail.com>
2022-08-11 22:30:29 +0000

    Completed prep-javascript-functions exercise

---
+++ prep-javascript-functions/main.js
@@ -0,0 +1,62 @@
+function addTwoNumbers(x, y) {
+  return x + y;
+}
+
+var sum = addTwoNumbers(4, 4);
+console.log('sum', sum);
+
+function convertHoursToMinutes(hours) {
+  return hours * 60;
+}
+
+var minutes = convertHoursToMinutes(5);
+console.log('minutes', minutes);
+
+function getGreeting(name) {
+  return 'Hello' + ' ' + name;
+}
+
+var greeting = getGreeting('Novi');
+console.log('greeting', greeting);
+
+function addAndMultiplyBy5(c, d) {
+  return (c + d) * 5;
+}
+
+var addAndMultiplyBy5result = addAndMultiplyBy5(3, 7);
+console.log('addAndMultiplyBy5result', addAndMultiplyBy5result);
+
+function multiplyAndDivideBy5(e, f) {
+  return (e * f) / 5;
+}
+
+var multiplyAndDivideBy5result = multiplyAndDivideBy5(10, 8);
+console.log('multiplyAndDivideBy5result', multiplyAndDivideBy5result);
+
+function subtractTwoNumbers(g, h) {
+  return (g - h);
+}
+
+var subtractTwoNumbersresult = subtractTwoNumbers(24, 9);
+console.log('subtractTwoNumbersresult', subtractTwoNumbersresult);
+
+function GetCircleCircumference(radius) {
+  return (2 * radius * 3.14);
+}
+
+var GetCircleCircumferenceresult = GetCircleCircumference(5);
+console.log('GetCircleCircumferenceresult', GetCircleCircumferenceresult);
+
+function getFullName(firstName, lastName) {
+  return firstName + ' ' + lastName;
+}
+
+var fullName = getFullName('Novi', 'Le');
+console.log('fullName', fullName);
+
+function cube(h) {
+  return (h * h * h);
+}
+
+var cuberesult = cube(5);
+console.log('cuberesult', cuberesult);
